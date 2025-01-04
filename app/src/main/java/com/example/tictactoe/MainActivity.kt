fc654e969d3b1ac81b0873947db7c9389955a31f
Liron Shriki <liron757@gmail.com>
2025-01-04 14:45:12 +0200

    added layout design

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,47 +1,33 @@
 package com.example.tictactoe
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.tictactoe.ui.theme.TictactoeTheme
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttons: Array<Array<Button>>
+    private lateinit var tvCurrentTurn: TextView
+    private lateinit var btnReset: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            TictactoeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+        buttons = Array(3) { row ->
+            Array(3) { col ->
+                val buttonId = resources.getIdentifier(
+                    "button_$row$col",
+                    "id",
+                    packageName
+                )
+                findViewById(buttonId)
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        btnReset = findViewById(R.id.btnReset)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TictactoeTheme {
-        Greeting("Android")
+        tvCurrentTurn = findViewById(R.id.tvCurrentTurn)
     }
 }
